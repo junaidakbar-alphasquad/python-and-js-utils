8062dce0b58c5c6401fd8f2f77f155904b51a76d
Junaid Akbar <junaidakbar.alphasquad@gmail.com>
2024-09-20 14:30:14 +0500

    modified:   nameUpdate.py

--- nameUpdate.py
+++ nameUpdate.py
@@ -9,7 +9,13 @@ for filename in os.listdir(directory):
     
     # Check if it's a file
     if os.path.isfile(file_path):
-            new_name = filename.replace(" ","-").lower() # filename[:-4] + "1.png"
-            new_path = os.path.join(directory, new_name)
-            os.rename(file_path, new_path)
-            # os.remove(file_path)
+        new_name = filename# filename[:-4] + "1.png"
+        try:
+            new_name = new_name.split("-")[1]# filename[:-4] + "1.png"
+        except IndexError:
+            print("Error: Invalid filename format")
+            
+        new_name = new_name.replace(" ","-").lower() # filename[:-4] + "1.png"
+        new_path = os.path.join(directory, new_name)
+        os.rename(file_path, new_path)
+        # os.remove(file_path)
